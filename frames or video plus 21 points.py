96f23578459e90582a8161e7f5c3841fc2710601
jackson0116 <101626015+jackson0116@users.noreply.github.com>
2023-08-03 21:28:35 +0800

    23/8/3

--- frames or video plus 21 points.py
+++ frames or video plus 21 points.py
@@ -16,54 +16,45 @@ def process_frame(frame):
     
     if results.multi_hand_landmarks:
         for hand_landmarks in results.multi_hand_landmarks:
-            mp_drawing.draw_landmarks(frame, hand_landmarks)
+            mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
 
     # 轉換為BGR格式
     frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
     
     return frame
     
-# 處理影片
-def process_video(video_path, output_path):
-    cap = cv2.VideoCapture(video_path)
-    fps = int(cap.get(cv2.CAP_PROP_FPS))
-    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+# 處理所有子資料夾中的圖片
+def process_directory(input_dir, output_dir):
+    for root, dirs, files in os.walk(input_dir):
+        for file_name in files:
+            if file_name.endswith('.jpg') or file_name.endswith('.png'):
+                input_path = os.path.join(root, file_name)
+                relative_path = os.path.relpath(input_path, input_dir)
+                output_path = os.path.join(output_dir, relative_path)
+                
+                # 建立輸出資料夾
+                os.makedirs(os.path.dirname(output_path), exist_ok=True)
 
-    out = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (width, height))
-
-    while cap.isOpened():
-        ret, frame = cap.read()
-
-        if ret:
-            processed_frame = process_frame(frame)
-            out.write(processed_frame)
-
-            cv2.imshow('frame', processed_frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-        else:
-            break
-
-    cap.release()
-    out.release()
-    cv2.destroyAllWindows()
-
+                frame = cv2.imread(input_path)
+                processed_frame = process_frame(frame)
+                cv2.imwrite(output_path, processed_frame)
+                print(file_name)
+# 使用函數，只需替換為你的輸入和輸出資料夾路徑
+process_directory('D:/project/picture/downtoup', 'D:/project/picture plus points/downtoup')
 # 資料夾中的所有影格進行處理
-for file_name in os.listdir('D:/project/picture/downup/00000'):
-    if file_name.endswith('.jpg') or file_name.endswith('.png'):
-        file_path = os.path.join('D:/project/picture/downup/00000', file_name)
-        
+#for file_name in os.listdir('D:/project/picture/downup/00000'):
+#    if file_name.endswith('.jpg') or file_name.endswith('.png'):
+ #       file_path = os.path.join('D:/project/picture/downup/00000', file_name)
+#
 
-
-        folder_path = 'D:/project/picture plus points/downup/00000'
-        if not os.path.isdir(folder_path):
-            os.mkdir(folder_path)
-            
-        output_path = os.path.join(folder_path, file_name)
-        frame = cv2.imread(file_path)
-        processed_frame = process_frame(frame)
-        cv2.imwrite(output_path, processed_frame)
-        print(file_name)
+ #       folder_path = 'D:/project/picture plus points/downup/00000'
+  #      if not os.path.isdir(folder_path):
+   #         os.mkdir(folder_path)
+    #        
+     #   output_path = os.path.join(folder_path, file_name)
+      # processed_frame = process_frame(frame)
+       # cv2.imwrite(output_path, processed_frame)
+        #print(file_name)
 
 
